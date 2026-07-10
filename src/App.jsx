cb2892a174c5f641fd51beeb2e0acd4397d3acae
João Vitor Ramos <joaovitorramosdealmeida99@gmail.com>
2026-07-10 13:21:44 -0300

    Build: Adicionar suporte para deploy no GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 
 
 function App() {
-  
+    return (
+        <div className="App">
+            <h1>Portfolio</h1>
+        </div>
+    )
 }
 
 export default App
