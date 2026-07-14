3bd8ff9d9e7b9fadbfa282dacf2ffb548b540df2
João Vitor Ramos <joaovitorramosdealmeida99@gmail.com>
2026-07-14 13:08:46 -0300

    fix: Centralizar conteudo dentro da secao header
    - ajustar espacamentos entre os elementos

--- src/pages/HomePage/Header/index.jsx
+++ src/pages/HomePage/Header/index.jsx
@@ -1,54 +1,75 @@
-import "./index.css";
+import "/src/global.css";
 import imageHeader from "../../../assets/image/image-header.svg";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function Header() {
   return (
     <header>
-      <nav>
-        <div id="navContainer">
-          <h2 id="logo">
+      <nav className="bg-primary-light">
+        <div className="content-container py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8 ">
+          <h2 className="text-3xl font-bold " >
             Web
-            <span id="DevLogo">Dev</span>
+            <span className="text-primary">Dev</span>
           </h2>
-          <div id="containerNavLinks">
-            <a className="navLink" href="#">
+          <div className="flex gap-x-3">
+            <a className="capitalize text-lg tracking-wide hover:text-blue-400 duration-300 " href="#">
               Home
             </a>
-            <a className="navLink" href="#">
+            <a className="capitalize text-lg tracking-wide hover:text-blue-400 duration-300 "  href="#">
               Skills
             </a>
-            <a className="navLink" href="#">
+            <a className="capitalize text-lg tracking-wide hover:text-blue-400 duration-300 "  href="#">
               Sobre
             </a>
-            <a className="navLink" href="#">
+            <a className="capitalize text-lg tracking-wide hover:text-blue-400 duration-300 "  href="#">
               Projetos
             </a>
           </div>
         </div>
       </nav>
-      <div id="headerContent">
-        <div id="headerContainer">
+      <div className="bg-primary-light py-24">
+        <div className="content-container grid md:grid-cols-2 items-center gap-8">
           <article id="ArticleDescription">
-            <h2 id="headerTitle">Eu sou João Vitor</h2>
-            <p id="headerSubtitle">Desenvolvedor Front-End</p>
-            <p id="headerDescription">Transformando ideias em realidade interativa</p>
+            <h2 className="text-5xl font-bold tracking-wider">Eu sou João Vitor</h2>
+            <p className="mt-4 text-3xl capitalize text-slate-700 tracking-wider">Desenvolvedor Front-End</p>
+            <p className="mt-2 text-lg capitalize text-slate-700 tracking-wide">
+              Transformando ideias em realidade interativa
+            </p>
 
-            <div id="containerSocialMedia">
-              <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-                <FaGithub />
+            <div className="flex gap-x-4 mt-4">
+              <a 
+                href="https://github.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub
+                className="h-8 w-8 text-slate-500 hover:black-300"
+                />
               </a>
-              <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                <FaLinkedin />
+              <a
+                href="https://linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin
+                className="h-8 w-8 text-slate-500 hover:black-300"
+                />
               </a>
-              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-                <FaTwitter />
+              <a
+                href="https://twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTwitter
+                className="h-8 w-8 text-slate-500 hover:black-300"
+                />
               </a>
             </div>
           </article>
-          <article id="ArticleImage">
-            <img id="imageHeader"
-              src= {imageHeader}
+          <article className="hidden md:block">
+            <img
+              className="h-80 lg:h-96"
+              src={imageHeader}
               alt="Foto de João Vitor Ramos"
             />
           </article>
