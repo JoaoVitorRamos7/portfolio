d94028a3bf1b021f6cf935fe0be9a7cc4a5586d5
João Vitor Ramos <joaovitorramosdealmeida99@gmail.com>
2026-07-15 17:23:24 -0300

    fix: Corrigir conteudo do header
    - Trocar trecho desenvolvedor front-end
    - Trocar trecho Transformando ideais em realidade interativa
    - Adicionar espaçamentos no navbar
    - Remover banner e adicionar foto
    - Adicionar João Vitor Ramos no h1
    - Trocar logo WebDev para JvDev

--- src/pages/HomePage/Header/index.jsx
+++ src/pages/HomePage/Header/index.jsx
@@ -1,17 +1,17 @@
 import "/src/global.css";
-import imageHeader from "../../../assets/image/image-header.svg";
+import imageHeader from "../../../assets/image/joao1.jpg";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function Header() {
   return (
     <header>
       <nav className="bg-primary-light">
-        <div className="content-container py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8 ">
+        <div className="content-container py-4 flex flex-col sm:flex-row sm:gap-x-24 sm:items-center sm:py-8 ">
           <h2 className="text-3xl font-bold " >
-            Web
+            Jv
             <span className="text-primary">Dev</span>
           </h2>
-          <div className="flex gap-x-3">
+          <div className="flex gap-x-4">
             <a className="capitalize text-lg tracking-wide hover:text-blue-400 duration-300 " href="#">
               Home
             </a>
@@ -27,13 +27,13 @@ function Header() {
           </div>
         </div>
       </nav>
-      <div className="bg-primary-light py-24">
+      <div className="bg-primary-light py-17">
         <div className="content-container grid md:grid-cols-2 items-center gap-8">
           <article id="ArticleDescription">
-            <h2 className="text-5xl font-bold tracking-wider">Eu sou João Vitor</h2>
+            <h2 className="text-5xl font-bold tracking-wider">João Vitor Ramos</h2>
             <p className="mt-4 text-3xl capitalize text-slate-700 tracking-wider">Desenvolvedor Front-End</p>
             <p className="mt-2 text-lg capitalize text-slate-700 tracking-wide">
-              Transformando ideias em realidade interativa
+              Desenvolvendo experiências digitais que unem design e performance.
             </p>
 
             <div className="flex gap-x-4 mt-4">
@@ -43,7 +43,7 @@ function Header() {
                 rel="noopener noreferrer"
               >
                 <FaGithub
-                className="h-8 w-8 text-slate-500 hover:black-300"
+                className="  h-11 w-11 text-slate-500 hover:black-300"
                 />
               </a>
               <a
@@ -52,27 +52,18 @@ function Header() {
                 rel="noopener noreferrer"
               >
                 <FaLinkedin
-                className="h-8 w-8 text-slate-500 hover:black-300"
-                />
-              </a>
-              <a
-                href="https://twitter.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaTwitter
-                className="h-8 w-8 text-slate-500 hover:black-300"
+                className="h-11 w-11 text-slate-500 hover:black-300"
                 />
               </a>
             </div>
           </article>
           <article className="hidden md:block">
             <img
-              className="h-80 lg:h-96"
+              className=" w-[240px] h-[320px] -translate-y-[10px] shadow-2xl rounded-3xl  lg:rounded-3xl  h-80 lg:h-[500px] lg:w-[375px] "
               src={imageHeader}
               alt="Foto de João Vitor Ramos"
-              width={412}
-              height={384}
+              height={500}
+              width={375}
             />
           </article>
         </div>
